836ecbd12cec1cc740a5f9631867d2ca76dbbc5b
PcFedora <evapozoleon@gmail.com>
2026-09-21 17:59:21 +0200

    Primer commit sin arrays

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+
+  //let contador = 0;
+  //metemos variablo contador en el STATE DEL COMPONENTE, usando hook useState()
+  // useState() devuelve un array con dos elementos: el valor actual del estado y una función para actualizarlo
+  
+  // const valorStateContador = useState(10); // valor inicial del contador es 10
+  // let contador=valorStateContador[0]; // valor actual del contador
+  // let setContador=valorStateContador[1]; // funcion modificadora del valor del contador
+  // console.log('valor devuelto por el hook useState() al meter variable contador  en el STATE DEL COMPONENTE: ', valorStateContador);
+  // console.log('- primera posicion del array: VALOR ACTUAL de la variable....', valorStateContador[0]);
+  // console.log('- segunda posicion del array: FUNCION MODIFICADORA de la variable....', valorStateContador[1].toString() );
+  
+  
+  //const [ contador, setContador ] = useState(10); // valor inicial del contador es 10
+
+  const [nombreUsuario, setNombreUsuario ] = useState('');//valor inicial del nombre de usuario es cadena vacia
+  const [email, setEmail] = useState('');
+  const [password, setPassword ] = useState('');
+  const [repassword, setRePassword ] = useState('');
+
+  //el hook useState te devuelve un ARRAY con dos elementos:
+  //- en la primera posicion esta el valor actual de la variable
+  //- en la segunda posicion te devuelve una FUNCION QUE PERMITE ACTUALIZAR/MODIFICAR su valor desde codigo
+ 
+
+  /*function clickBoton(ev){
+     console.log('evento0 interceptado en funcion click del boton...', ev);
+     //contador++;
+     //valorStateContador[1](valorStateContador[0]+1); //llamamos a la funcion modificadora del valor del contador, pasando el nuevo valor
+     setContador(contador+1); //llamamos a la funcion modificadora del valor del contador, pasando el nuevo valor
+     console.log('contador vale ahora: ', contador);
+
+  }*/
+
+  function changeNombre(ev){
+    console.log('evento change del input-nombre, el valor actual de la caja de texto es...', ev.target.value);
+    setNombreUsuario(ev.target.value); //llamo a la funcion modificadora d esta variable del state y asigno un nuevo valor
+    console.log('nombre de usuario vale ahora: ', nombreUsuario);
+  }
+
+  function changeEmail(ev){
+    console.log('evento change del input-email, el valor actual de la caja de texto es...', ev.target.value);
+    setNombreUsuario(ev.target.value); //llamo a la funcion modificadora d esta variable del state y asigno un nuevo valor
+    console.log('email vale ahora: ', email);
+  }
+
+  function changePassword(ev){
+    console.log('evento change del input-email, el valor actual de la caja de texto es...', ev.target.value);
+    setNombreUsuario(ev.target.value); //llamo a la funcion modificadora d esta variable del state y asigno un nuevo valor
+    console.log('password vale ahora: ', password);
+  }
+
+  function changeRePassword(ev){
+    console.log('evento change del input-email, el valor actual de la caja de texto es...', ev.target.value);
+    setNombreUsuario(ev.target.value); //llamo a la funcion modificadora d esta variable del state y asigno un nuevo valor
+    console.log('repassword vale ahora: ', repassword);
+  }
+
+  return (
+    <>
+      <h1>Vamos a comprobar lo que es el STATE de un componente de REACT</h1>
+      {/* <p>Contador vale: { valorStateContador[0] }</p>  <p>Contador vale: { contador }</p>
+      <button className="btn btn-primary" onClick={ clickBoton }> INCREMENTA EL CONTADOR</button>
+    */}
+    <div className = "m-4">
+      <label className= "form-label" htmlFor="inputNombre">Nombre Usuario: {nombreUsuario} </label>
+      <input type = "text"
+              id = "inputNombre"
+              className = "form-control"
+              placeholder = "Nombre*"
+              onChange = {changeNombre}/>
+      
+      </div>
+
+      <div className = "m-4">
+      <label className= "form-label" htmlFor="inputNombre">Email: {email} </label>
+      <input type = "text"
+              id = "inputEmail"
+              className = "form-control"
+              placeholder = "Email*"
+              onChange = {changeEmail}/>
+     
+      </div>
+
+      <div className = "m-4">
+      <label className= "form-label" htmlFor="inputNombre">Password: {password} </label>
+      <input type = "text"
+              id = "inputPassword"
+              className = "form-control"
+              placeholder = "Password*"
+              onChange = {changePassword}/>
+      
+      </div>
+
+      <div className = "m-4">
+      <label className= "form-label" htmlFor="inputNombre">RePassword: {repassword} </label>
+      <input type = "text"
+              id = "inputPassword"
+              className = "form-control"
+              placeholder = "RePassword*"
+              onChange = {changeRePassword}/>
+     
+      </div>
+
+
+     </>
+  )
+}
+
+export default App 
